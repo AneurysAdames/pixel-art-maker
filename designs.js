e8534f10d52073ac3002bd25dccf477e03c9a46b
Jessica Sutton <jmsutton0703@gmail.com>
2018-03-27 20:48:24 -0500

    working

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
-// Select color input
-// Select size input
+$("document").ready(function() {
 
-// When size is submitted by the user, call makeGrid()
+  // Select color input
+  let color = $('#colorPicker').val();
 
-function makeGrid() {
+  // Select size
+  let rows = $('#inputHeight').val();
+  let cols = $('#inputWidth').val();
 
-// Your code goes here!
+  // When size is submitted by the user,
+  // the grid is cleared and the function makeGrid() is called
+  $('input[type="submit"]').click(function(event) {
+    event.preventDefault();
+    $('tr').remove();
+    makeGrid();
+  });
 
-}
+  // Creates a grid
+  function makeGrid() {
+    let rows = $('#inputHeight').val();
+    let cols = $('#inputWidth').val();
+
+    for (let i = 1; i <= rows; i++) {
+      $('table').append('<tr></tr>');
+      for (let j = 1; j <= cols; j++) {
+        $('tr:last').append('<td></td>');
+      }
+    }
+    event.preventDefault();
+
+    // Assigns 'td' the class of 'cells'
+    $('td').attr("class", 'cells');
+
+    // Saves color selected by user in var color
+    // and allows user to color the cells they click on
+    $('.cells').click(function(event) {
+      var color = $('#colorPicker').val();
+      $(event.target).css('background-color', color);
+    });
+  }
+});
